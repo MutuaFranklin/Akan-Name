190dea5b2972a15088e400e6b94d5a49bec73d02
MutuaFranklin <franklin.mutua@student.moringaschool.com>
2021-06-11 10:13:55 +0300

    JS Akan Name

--- static/js/main.js
+++ static/js/main.js
@@ -47,8 +47,72 @@ function getBirthDetails(){
 
     var day = ( ( ( (cc/4) -2 * cc-1) + ((5* yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7).toFixed();
 
+// Day name and Akan name
+// sex 1 = male. sex 2 = female
 
-
+    if (day == 0){
+        dayName = "Sunday";
+        if (sex == 1){
+            akanName = "Kwasi";
+        }
+        else{
+            akanName = "Akosua";
+        }
+    }
+    else if (day == 1){
+        dayName = "Monday";
+        if (sex == 1){
+            akanName = "Kwadwo";
+        }
+        else{
+            akanName = "Adwoa";
+        }
+    }
+    else if (day == 2){
+        dayName = "Tuesday";
+        if (sex == 1){
+            akanName = "Kwabena";
+        }
+        else{
+            akanName = "Abena";
+        }
+    }
+    else if (day == 3){
+        dayName = "Wednesday";
+        if (sex == 1){
+            akanName = "Kwaku";
+        }
+        else{
+            akanName = "Akua";
+        }
+    }
+    else if (day == 4){
+        dayName = "Thursday";
+        if (sex == 1){
+            akanName = "Yaw";
+        }
+        else{
+            akanName = "Yaa";
+        }
+    }
+    else if (day == 5){
+        dayName = "Friday";
+        if (sex == 1){
+            akanName = "Kofi";
+        }
+        else{
+            akanName = "Afua";
+        }
+    }
+    else if (day == 6){
+        dayName = "Saturday";
+        if (sex == 1){
+            akanName = "Kwame";
+        }
+        else{
+            akanName = "Ama";
+        }
+    }
 
 
         
